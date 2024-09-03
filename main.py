ea6611012a376c5d78cf8566a981bdd1e40a4db8
Ken980 <kenboy9800@gmail.com>
2024-09-03 18:01:55 +0700

    test extracting data

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8m-pose.pt")
 
-result = model(source=0, show=True, conf=0.3, save=True)
+results = model(source="test/1.jpg", show=False, conf=0.3, save=False, stream=False)
+
+
+for result in results:
+    keypoints = result.keypoints
+    print(result.keypoints.xy)
+
+
